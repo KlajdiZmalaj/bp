6ca484dc6abb4a49ee4ee527b958a47fe9d75623
nadia <nadia@altechsolutions.al>
2019-09-06 18:45:18 +0200

    mg

---
+++ build/precache-manifest.51eed2c3eb44ea939f7fef98010a9518.js
@@ -0,0 +1,250 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c189123c4e8cd698f2c202dc55601c1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1dd545f6767986ee6d6",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b86730cccc4de681609b",
+    "url": "/static/css/main.6f25f2be.chunk.css"
+  },
+  {
+    "revision": "d1dd545f6767986ee6d6",
+    "url": "/static/js/2.06ccb564.chunk.js"
+  },
+  {
+    "revision": "b86730cccc4de681609b",
+    "url": "/static/js/main.fb68791e.chunk.js"
+  },
+  {
+    "revision": "73898f26ee52d8e6f798",
+    "url": "/static/js/runtime~main.e83d5c72.js"
+  },
+  {
+    "revision": "c4274b9909ccd9637cda9f9af53e066a",
+    "url": "/static/media/Car.c4274b99.svg"
+  },
+  {
+    "revision": "f43a5811927d0d2f48bb5c51bcc6e0f3",
+    "url": "/static/media/Sky_Italia_-_Logo_2018.f43a5811.svg"
+  },
+  {
+    "revision": "043a876dacbdc25546a32fbad547eb71",
+    "url": "/static/media/Stanleybet_logo_international.043a876d.svg"
+  },
+  {
+    "revision": "5a86dd88182a12cf3b7a3d93a3c7bebd",
+    "url": "/static/media/TIM_logo_2016.5a86dd88.svg"
+  },
+  {
+    "revision": "4ea5730b5569b16a2ff17a746d1ee31f",
+    "url": "/static/media/admAZZ.4ea5730b.svg"
+  },
+  {
+    "revision": "7501735bf6880af4526236176a76e298",
+    "url": "/static/media/analysis.7501735b.svg"
+  },
+  {
+    "revision": "2e335f4fcd87077452b7d3962976ab57",
+    "url": "/static/media/barcode.2e335f4f.svg"
+  },
+  {
+    "revision": "2430448cb8ab904049546f99a1f5efac",
+    "url": "/static/media/bill-blue.2430448c.svg"
+  },
+  {
+    "revision": "3cf77a7284522cda9895a61455c6a410",
+    "url": "/static/media/bill-dark.3cf77a72.svg"
+  },
+  {
+    "revision": "39de4ee547925cf37d3a273e36bb34e9",
+    "url": "/static/media/bill-orange.39de4ee5.svg"
+  },
+  {
+    "revision": "5d3ff9276c4d498f37226127f4d9fd7b",
+    "url": "/static/media/bill.5d3ff927.svg"
+  },
+  {
+    "revision": "f9751a3ae3d3f9bd0afda833185688df",
+    "url": "/static/media/bitcoingreen.f9751a3a.svg"
+  },
+  {
+    "revision": "9574ee9d6f6b64cc3da2bac0115baccf",
+    "url": "/static/media/bitcoinorange.9574ee9d.svg"
+  },
+  {
+    "revision": "ed3b3b263aff6ffabbe51f7beec56e93",
+    "url": "/static/media/blueWig.ed3b3b26.png"
+  },
+  {
+    "revision": "a301933dc9e0ed7329e2e61c13b36a09",
+    "url": "/static/media/cancelMob.a301933d.svg"
+  },
+  {
+    "revision": "084b07b6ad6f3aa19ba06ed2873d4f36",
+    "url": "/static/media/chat.084b07b6.svg"
+  },
+  {
+    "revision": "eef8a7dd7fcab20dbcb4f409879d2441",
+    "url": "/static/media/click.eef8a7dd.svg"
+  },
+  {
+    "revision": "14270c67c6d581b91756a891be6783ab",
+    "url": "/static/media/coop.14270c67.svg"
+  },
+  {
+    "revision": "811082978a02c651e439f059624c023d",
+    "url": "/static/media/dashcoin.81108297.svg"
+  },
+  {
+    "revision": "41e47dea905d08d340cc3d5ba39cfc7a",
+    "url": "/static/media/employee.41e47dea.svg"
+  },
+  {
+    "revision": "03c3d0b21b65b66998b4cfb874f568d2",
+    "url": "/static/media/ethereum.03c3d0b2.svg"
+  },
+  {
+    "revision": "15d993cf979c19fe0585d5ba3c96731e",
+    "url": "/static/media/fa-brands-400.15d993cf.ttf"
+  },
+  {
+    "revision": "3aa9793f23d045a08e58c222fbc72187",
+    "url": "/static/media/fa-brands-400.3aa9793f.eot"
+  },
+  {
+    "revision": "6672c795cfdfdd5448f26eb8a1fa0086",
+    "url": "/static/media/fa-brands-400.6672c795.woff2"
+  },
+  {
+    "revision": "791dcd85200f501da87ba50a18e04dda",
+    "url": "/static/media/fa-brands-400.791dcd85.svg"
+  },
+  {
+    "revision": "d0fab90c876b3b3f74bd48b22d993010",
+    "url": "/static/media/fa-brands-400.d0fab90c.woff"
+  },
+  {
+    "revision": "3491ffc06c82fb0aaa0713042e48f4e9",
+    "url": "/static/media/fa-light-300.3491ffc0.eot"
+  },
+  {
+    "revision": "61d5c7a7767e7411310730d89cb5b55a",
+    "url": "/static/media/fa-light-300.61d5c7a7.woff"
+  },
+  {
+    "revision": "67cf9b9845e2bf8be3dba8a93fa07134",
+    "url": "/static/media/fa-light-300.67cf9b98.woff2"
+  },
+  {
+    "revision": "cc8d4b8a70fb96f9f61d265a70f3e4f3",
+    "url": "/static/media/fa-light-300.cc8d4b8a.svg"
+  },
+  {
+    "revision": "dc495f3603c0c200e3671d15c1d2daa1",
+    "url": "/static/media/fa-light-300.dc495f36.ttf"
+  },
+  {
+    "revision": "5b511a002256a7c687a166eb602b7ee7",
+    "url": "/static/media/fa-regular-400.5b511a00.woff2"
+  },
+  {
+    "revision": "7f16528e51df6bdeb1e1ccab692063e7",
+    "url": "/static/media/fa-regular-400.7f16528e.ttf"
+  },
+  {
+    "revision": "c01203d3c64b1d9de4181c390bde93d2",
+    "url": "/static/media/fa-regular-400.c01203d3.eot"
+  },
+  {
+    "revision": "eaa09d821722974cd7cd5b22bc4850f1",
+    "url": "/static/media/fa-regular-400.eaa09d82.woff"
+  },
+  {
+    "revision": "fe738e6a0afd7b7f5a4b35ef08d38a97",
+    "url": "/static/media/fa-regular-400.fe738e6a.svg"
+  },
+  {
+    "revision": "120d3a6c089bcf94fec12f99c651e84d",
+    "url": "/static/media/fa-solid-900.120d3a6c.ttf"
+  },
+  {
+    "revision": "428a9a696351bef3466353d7ed843224",
+    "url": "/static/media/fa-solid-900.428a9a69.svg"
+  },
+  {
+    "revision": "77cb90ddffd18fc9af390d8f4391d3bf",
+    "url": "/static/media/fa-solid-900.77cb90dd.woff"
+  },
+  {
+    "revision": "af21a74dc3cca53d83e2131b73509225",
+    "url": "/static/media/fa-solid-900.af21a74d.eot"
+  },
+  {
+    "revision": "b650897c484f29df93474098e5dde0ef",
+    "url": "/static/media/fa-solid-900.b650897c.woff2"
+  },
+  {
+    "revision": "4cb8ea572aa032291a8dc2c64732a0a0",
+    "url": "/static/media/gears.4cb8ea57.svg"
+  },
+  {
+    "revision": "326322f1f3307452eef5ad920e2ad21d",
+    "url": "/static/media/greenWig.326322f1.png"
+  },
+  {
+    "revision": "118c7013e4a9625f3986e2e37a2a4dc4",
+    "url": "/static/media/litecoin.118c7013.svg"
+  },
+  {
+    "revision": "935e78d1e394a91643fc030436a2c951",
+    "url": "/static/media/logo.935e78d1.svg"
+  },
+  {
+    "revision": "0b459c0335274387065fc32452539a9f",
+    "url": "/static/media/orangeWig.0b459c03.png"
+  },
+  {
+    "revision": "a5da3f6d041da27b6e84354898f4e113",
+    "url": "/static/media/paysafe.a5da3f6d.svg"
+  },
+  {
+    "revision": "935d1ef0e630827c113017c0bbc01f59",
+    "url": "/static/media/posteitalia.935d1ef0.svg"
+  },
+  {
+    "revision": "d11f23404297dc37f81159e06db43e57",
+    "url": "/static/media/postpayimg.d11f2340.svg"
+  },
+  {
+    "revision": "208b5c66c62888688a2b4e26ebc01e53",
+    "url": "/static/media/receipt.208b5c66.svg"
+  },
+  {
+    "revision": "114c92fcf8751f64cc8a458432bba503",
+    "url": "/static/media/riple.114c92fc.svg"
+  },
+  {
+    "revision": "6668785d494a28216f07172b69d4bc46",
+    "url": "/static/media/shop.6668785d.svg"
+  },
+  {
+    "revision": "c9098a057591f85d3da8bb37ddb8aa4e",
+    "url": "/static/media/three-logo.c9098a05.svg"
+  },
+  {
+    "revision": "191417e681f4ea4d8e6d86b5d3d129c4",
+    "url": "/static/media/tickets.191417e6.svg"
+  },
+  {
+    "revision": "d169a67514184ccdf92509e0e1270a6f",
+    "url": "/static/media/uparrow.d169a675.svg"
+  },
+  {
+    "revision": "3dfbfb56df37b759814e84e0c924dbb8",
+    "url": "/static/media/wind-logo-svg-vector.3dfbfb56.svg"
+  }
+]);
