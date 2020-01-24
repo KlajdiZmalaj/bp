91fa5534f714153c51b124511513ee2907101e8d
nadia <nadia@altechsolutions.al>
2020-01-24 18:03:51 +0100

    ch

--- src/redux-store/sagas/index.js
+++ src/redux-store/sagas/index.js
@@ -15,7 +15,7 @@ import { getServices } from "./MainSagas";
 
 export default function* root() {
   yield all([
-    // AUTH
+    // AUTH h
     takeLatest(AuthTypes.SIGN_IN_BY_EMAIL, signInByEmail),
     takeLatest(AuthTypes.GET_ACCOUNT_INFO, getAccountInfo),
     takeLatest(AuthTypes.LOG_OUT, logOut),
