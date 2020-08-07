b373c778d45de139824d30fb70e2fdf54ef05dc3
Klajdi <klajdizmalaj@icloud.com>
2020-08-07 15:39:55 +0200

    m

--- src/shared-components/Azioni/Azioni.jsx
+++ src/shared-components/Azioni/Azioni.jsx
@@ -11,10 +11,10 @@ class Azioni extends Component {
   render() {
     const { active, accountInfo } = this.props;
     const params = {
-      spaceBetween: 10,
-      loop: false,
-      slidesPerView: 6,
-      freeMode: false,
+      mousewheel: true,
+      centeredSlides:
+        active === "annunci" || active === "account-info" ? false : true,
+
       breakpoints: {
         // when window width is >= 320px
         320: {
@@ -36,7 +36,7 @@ class Azioni extends Component {
         item.displayRole,
         get(accountInfo, "profile.role.name")
       ) ? (
-        <div key={Math.random() + item.id}>
+        <div key={item.link}>
           <a href={"#/" + item.link}>
             <div
               className={
@@ -52,7 +52,7 @@ class Azioni extends Component {
           </a>
         </div>
       ) : (
-        <div key={Math.random() + item.id + ind} className="d-none" />
+        <></>
       );
     });
     return (
@@ -65,10 +65,8 @@ class Azioni extends Component {
           >
             <Swiper
               key={Math.random() + slides.id * 2}
+              activeSlideKey={active}
               {...params}
-              // activeSlideKey={`${
-              //   active === "carica-conto" || active === "configura" ? "3" : "0"
-              // }`}
             >
               {slides}
             </Swiper>
