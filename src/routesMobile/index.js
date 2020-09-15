4b247172d1153b669a54a3db6c928de9e58767e4
Klajdi <klajdizmalaj@icloud.com>
2020-09-15 12:03:20 +0200

    m

--- src/routesMobile/index.js
+++ src/routesMobile/index.js
@@ -1,2 +1,3 @@
 export { default as DashboardMobile } from "./views/DashboardMobile";
 export { default as ConfiguraMobile } from "./views/ConfiguraMobile";
+export { default as PrenotazioneMobile } from "./views/PrenotazioneMobile";
