8da1a42262045c033dbe04143545d9afa760c84d
Klajdi <klajdizmalaj@icloud.com>
2020-09-15 18:29:47 +0200

    m

--- src/shared-componentsMobile/Header/TabBody.jsx
+++ src/shared-componentsMobile/Header/TabBody.jsx
@@ -3,7 +3,7 @@ import { azioni } from "config";
 import { get, includes } from "lodash";
 import { Tooltip } from "antd";
 import { open } from "shared-components/Chat/Chat";
-export default ({ tab, accountInfo, logOut }) => {
+export default ({ tab, accountInfo, logOut, allServices, setMenu }) => {
   return tab === 1 ? (
     <div className="tabBody body1">
       {accountInfo?.profile ? (
@@ -70,6 +70,68 @@ export default ({ tab, accountInfo, logOut }) => {
       </div>
     </div>
   ) : (
-    <div className="tabBody body2"></div>
+    <div className="tabBody body2">
+      {accountInfo?.profile ? (
+        <div className="tabBody--item">
+          <i className="fal fa-user-circle" aria-hidden="true"></i>
+
+          <Tooltip title={accountInfo?.profile?.name}>
+            {" "}
+            <div className="nameUser">{accountInfo?.profile?.name}</div>
+          </Tooltip>
+
+          <span>{accountInfo?.profile?.wallet}€</span>
+        </div>
+      ) : (
+        <div
+          onClick={() => {
+            window.location.hash = "login";
+          }}
+          className="tabBody--item active"
+        >
+          <i className="fal fa-sign-in" aria-hidden="true"></i>
+          Login
+        </div>
+      )}
+      {(allServices?.categories || []).map((category) => (
+        <div
+          className="tabBody--item"
+          onClick={() => {
+            setMenu("out");
+            setTimeout(() => {
+              setMenu(false);
+              const el = document.querySelector(`#${category.name}`);
+              if (category.name === "BGLT" || category.name === "VSRE") {
+                window.location.hash = "forms";
+              } else {
+                if (el) {
+                  setTimeout(() => {
+                    el.click();
+                    el.scrollIntoView({
+                      behavior: "smooth",
+                    });
+                  }, 100);
+                } else {
+                  window.location.hash = "dashboard";
+                  setTimeout(() => {
+                    const el = document.querySelector(`#${category.name}`);
+                    if (el) {
+                      el.click();
+                      el.scrollIntoView({
+                        behavior: "smooth",
+                      });
+                    }
+                  }, 300);
+                }
+              }
+            }, 500);
+          }}
+        >
+          <i className={`fal ${category.name}`} aria-hidden="true"></i>{" "}
+          <span> {category.full_name}</span>
+          <i className="fal fa-chevron-right" aria-hidden="true"></i>{" "}
+        </div>
+      ))}
+    </div>
   );
 };
