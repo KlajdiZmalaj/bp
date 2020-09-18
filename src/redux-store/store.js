0f44d650d6d9cac8609eedeee79e8a67dfbb9c2c
Klajdi <klajdizmalaj@icloud.com>
2020-09-18 11:13:33 +0200

    m

--- src/redux-store/store.js
+++ src/redux-store/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, combineReducers } from "redux";
+import { createStore, applyMiddleware, combineReducers, compose } from "redux";
 import createSagaMiddleware from "redux-saga";
 
 import { reducer as AuthReducer } from "./models/auth";
@@ -12,20 +12,25 @@ const rootReducer = combineReducers({
 export default function configureStore() {
   const loggerMiddleware = (store) => (next) => (action) => {
     const returnValue = next(action);
-    if (console.group) {
-      console.group(action.type);
-      console.log("%c action", "color: #03A9F4", action);
-      console.log("%c newState", "color: #03A9F4", store.getState());
-      console.groupEnd();
-    }
+    // if (console.group) {
+    //   console.group(action.type);
+    //   console.log("%c action", "color: #03A9F4", action);
+    //   console.log("%c newState", "color: #03A9F4", store.getState());
+    //   console.groupEnd();
+    // }
     return returnValue;
   };
   const sagaMiddleware = createSagaMiddleware();
 
   const middlewares = [sagaMiddleware, loggerMiddleware];
+  const composeEnhancers =
+    typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+      : compose;
 
+  const enhancer = composeEnhancers(applyMiddleware(...middlewares));
   return {
-    ...createStore(rootReducer, applyMiddleware(...middlewares)),
+    ...createStore(rootReducer, enhancer),
     runSaga: sagaMiddleware.run,
   };
 }
