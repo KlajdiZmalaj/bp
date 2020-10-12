4950d991fa36927d02236f3388d3c74722ddffe5
Klajdi <klajdizmalaj@icloud.com>
2020-10-12 12:12:48 +0200

    m

--- src/shared-components/NewBarcodeScanner/index.jsx
+++ src/shared-components/NewBarcodeScanner/index.jsx
@@ -21,13 +21,9 @@ class Scanner extends Component {
         decoder: {
           readers: [
             "code_128_reader",
-            "ean_reader",
-            "ean_8_reader",
             "code_39_reader",
             "code_39_vin_reader",
             "codabar_reader",
-            "upc_reader",
-            "upc_e_reader",
             "code_93_reader",
           ],
         },
