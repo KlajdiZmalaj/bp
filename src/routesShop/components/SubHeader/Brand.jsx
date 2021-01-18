f968ad7cb4efdd6e57183de5498001b6adb36186
nadia <nadia@altechsolutions.al>
2021-01-18 15:04:13 +0100

    ch

--- src/routesShop/components/SubHeader/Brand.jsx
+++ src/routesShop/components/SubHeader/Brand.jsx
@@ -1,37 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import ShopActions from "redux-store/models/shop";
-import AuthActions from "redux-store/models/auth";
 
 import "./style.css";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
-import { get } from "lodash";
-import { Radio, Form } from "antd";
 
-import images from "themes/images";
-
-const Brand = ({
-  getProductsList,
-  history,
-  brand,
-  accountInfo,
-  cat,
-  itemS,
-}) => {
-  useEffect(() => {
-    // setData({
-    //   ...formData,
-    //   name: accountInfo?.profile?.name?.split?.(" ")?.[0],
-    //   last_name: accountInfo?.profile?.name?.split?.(" ")?.[1],
-    //   email: accountInfo?.profile?.email,
-    //   cap: itemsCart?.user_data?.postcode,
-    // });
-  }, [
-    accountInfo,
-    // match.params.id,
-    // match.params.supp,
-  ]);
-
+const Brand = ({ getProductsList, history, brand, cat, itemS }) => {
   const [cost, setCost] = useState(false);
 
   return (
@@ -48,7 +22,7 @@ const Brand = ({
       }}
     >
       <img
-        src={brand?.url || images["placeholder"]}
+        src={brand?.url}
         className={cost && "hidden"}
         onError={(e) => {
           e.target.onerror = null;
@@ -65,6 +39,4 @@ const mstp = (state) => ({
   accountInfo: state.auth.accountInfo,
 });
 
-export default withRouter(
-  connect(mstp, { ...ShopActions, AuthActions })(Form.create()(Brand))
-);
+export default withRouter(connect(mstp, { ...ShopActions })(Brand));
