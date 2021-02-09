04074ca078bdb1a51dc840a3f805a2e830cd9545
Klajdi <klajdizmalaj@icloud.com>
2021-02-09 11:07:01 +0100

    m

--- src/Root.jsx
+++ src/Root.jsx
@@ -101,9 +101,7 @@ class Root extends React.Component {
     var divInstall = document.getElementById("PWA");
     window.addEventListener("beforeinstallprompt", (event) => {
       console.log("👍", "beforeinstallprompt", event);
-      // Stash the event so it can be triggered later.
       window.deferredPrompt = event;
-      // Remove the 'hidden' class from the install button container
 
       divInstall.classList.toggle("hidden", false);
     });
@@ -111,23 +109,24 @@ class Root extends React.Component {
       console.log("👍", "butInstall-clicked");
       const promptEvent = window.deferredPrompt;
       if (!promptEvent) {
-        // The deferred prompt isn't available.
         return;
       }
-      // Show the install prompt.
       promptEvent.prompt();
-      // Log the result
       promptEvent.userChoice.then((result) => {
         console.log("👍", "userChoice", result);
-        // Reset the deferred prompt variable, since
-        // prompt() can only be called once.
+        if (result) {
+          document.body.classList.add(`pwa-${result.platform}`);
+        }
         window.deferredPrompt = null;
-        // Hide the install button.
         divInstall.classList.toggle("hidden", true);
       });
     });
     window.addEventListener("appinstalled", (event) => {
       console.log("👍", "appinstalled", event);
+      notification["success"]({
+        message: "l'applicazione è stato installato",
+        placement: "bottomLeft",
+      });
     });
   }
   componentWillUnmount() {
